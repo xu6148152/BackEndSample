23ac1f5b58b0b1810f3bff86af6a70c979ace991
xu6148152 <xu6148152@gmail.com>
2017-11-18 15:30:58 +0800

    Add spring cloud bus

---
+++ SpringCloudBus/config-client-eureka-kafka/src/main/java/com/binea/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.binea;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(DemoApplication.class).web(true).run(args);
+    }
+}
---
+++ SpringCloudBus/config-client-eureka-kafka/src/main/java/com/binea/web/TestController.java
@@ -0,0 +1,32 @@
+package com.binea.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by binea
+ * Date: 18/11/2017
+ * TIME: 2:59 PM
+ */
+@RefreshScope
+@RestController
+public class TestController {
+
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+}
