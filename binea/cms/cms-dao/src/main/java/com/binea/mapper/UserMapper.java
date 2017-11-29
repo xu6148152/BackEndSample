a5ff2e38a4adc0fe293c97ca73a7a869e29884e4
xu6148152 <xu6148152@gmail.com>
2017-11-29 22:19:03 +0800

    Add cms dao

---
+++ binea/cms/cms-dao/src/main/java/com/binea/mapper/UserMapper.java
@@ -0,0 +1,43 @@
+package com.binea.mapper;
+
+import com.binea.model.User;
+import com.binea.model.UserExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * Created by binea
+ * Date: 29/11/2017
+ * TIME: 10:16 PM
+ */
+
+public interface UserMapper {
+    int countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExampleWithBLOBs(UserExample example);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKeyWithBLOBs(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ binea/cms/cms-dao/src/main/java/com/binea/model/Book.java
@@ -0,0 +1,38 @@
+package com.binea.model;
+
+/**
+ * Created by binea
+ * Date: 29/11/2017
+ * TIME: 10:11 PM
+ */
+public class Book {
+    private Integer id;
+
+    private Integer userid;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
