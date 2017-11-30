c9801e07e83ea01adca37bcbdec4c8aa262f57d7
xu6148152 <xu6148152@gmail.com>
2017-11-30 22:31:17 +0800

    Add cms-web

---
+++ binea/cms/cms-web/src/main/java/com/binea/controller/BaseController.java
@@ -0,0 +1,15 @@
+package com.binea.controller;
+
+/**
+ * Created by binea
+ * Date: 30/11/2017
+ * TIME: 10:17 PM
+ */
+public class BaseController {
+
+    public static final String RESULT = "result";
+    public static final String DATA = "data";
+    public static final String SUCCESS = "success";
+    public static final String FAILED = "failed";
+
+}
