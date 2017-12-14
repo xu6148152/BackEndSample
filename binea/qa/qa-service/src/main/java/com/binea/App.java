4788d363db18fc7d2f54a841f1c907f0f58d40ef
xu6148152 <xu6148152@gmail.com>
2017-12-14 22:34:49 +0800

    Add new module

---
+++ binea/qa/qa-service/src/main/java/com/binea/App.java
@@ -0,0 +1,13 @@
+package com.binea;
+
+/**
+ * Created by binea
+ * Date: 13/12/2017
+ * TIME: 10:38 PM
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ binea/upms/upms-service/src/main/java/com/binea/upms/service/App.java
@@ -0,0 +1,13 @@
+package java.com.binea.upms.service;
+
+/**
+ * Created by binea
+ * Date: 14/12/2017
+ * TIME: 10:32 PM
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
