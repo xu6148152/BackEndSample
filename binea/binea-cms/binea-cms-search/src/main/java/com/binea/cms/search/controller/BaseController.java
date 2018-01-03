8fe30866fc1d75172b7c80eefcb7e79c7577abef
xu6148152 <xu6148152@gmail.com>
2018-01-03 23:00:09 +0800

    Refactor

--- binea/binea-cms/binea-cms-search/src/main/java/com/binea/cms/search/controller/BaseController.java
+++ binea/binea-cms/binea-cms-search/src/main/java/com/binea/cms/search/controller/BaseController.java
@@ -1,15 +1,13 @@
-package com.binea.controller;
+package com.binea.cms.search.controller;
 
 /**
  * Created by binea
- * Date: 30/11/2017
- * TIME: 10:17 PM
+ * Date: 3/1/2018
+ * TIME: 10:54 PM
  */
 public class BaseController {
-
     public static final String RESULT = "result";
     public static final String DATA = "data";
     public static final String SUCCESS = "success";
     public static final String FAILED = "failed";
-
 }
