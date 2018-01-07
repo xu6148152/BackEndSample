31bfd2657f589cb460bc5959a4359cf9e25fc1b8
xu6148152 <xu6148152@gmail.com>
2018-01-07 19:19:01 +0800

    Refactor

--- SpringBootSample/SpringWeb/src/main/java/com/binea/web/HelloController.java
+++ SpringBootSample/SpringWeb/src/main/java/com/binea/web/HelloController.java
@@ -3,11 +3,11 @@ package com.binea.web;
 import com.binea.exception.MyException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by binea
