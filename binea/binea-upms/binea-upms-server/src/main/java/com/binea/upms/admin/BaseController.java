926a37490bd3bbc75dbb72a5658277943fb1a2f1
xu6148152 <xu6148152@gmail.com>
2018-01-15 22:50:29 +0800

    Add upms client and server

---
+++ binea/binea-upms/binea-upms-server/src/main/java/com/binea/upms/admin/BaseController.java
@@ -0,0 +1,13 @@
+package com.binea.upms.admin;
+
+/**
+ * Created by binea
+ * Date: 15/1/2018
+ * TIME: 10:49 PM
+ */
+public class BaseController {
+    public static final String RESULT = "result";
+    public static final String DATA = "data";
+    public static final String SUCCESS = "success";
+    public static final String FAILED = "failed";
+}
