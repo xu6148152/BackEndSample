d001f915a3c7a7b02f1f2cfee85731dcc1dd0a2a
xu6148152 <xu6148152@gmail.com>
2018-02-04 21:45:38 +0800

    Add LogEvent for udp protocal

---
+++ NettySample/src/main/java/ch13/LogEventDecoder.java
@@ -0,0 +1,26 @@
+package ch13;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.handler.codec.MessageToMessageDecoder;
+import io.netty.util.CharsetUtil;
+
+import java.util.List;
+
+/**
+ * Created by binea
+ * Date: 4/2/2018
+ * TIME: 9:33 PM
+ */
+public class LogEventDecoder extends MessageToMessageDecoder<DatagramPacket> {
+    @Override
+    protected void decode(ChannelHandlerContext ctx, DatagramPacket msg, List<Object> out) throws Exception {
+        ByteBuf data = msg.content();
+        int idx = data.indexOf(0, data.readableBytes(), LogEvent.SEPARATOR);
+        String filename = data.slice(0, idx).toString(CharsetUtil.UTF_8);
+        String logMsg = data.slice(idx + 1, data.readableBytes()).toString(CharsetUtil.UTF_8);
+        LogEvent event = new LogEvent(msg.sender(), System.currentTimeMillis(), filename, logMsg);
+        out.add(event);
+    }
+}
---
+++ NettySample/src/main/java/ch13/LogEventHandler.java
@@ -0,0 +1,31 @@
+package ch13;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * Created by binea
+ * Date: 4/2/2018
+ * TIME: 9:40 PM
+ */
+public class LogEventHandler extends SimpleChannelInboundHandler<LogEvent> {
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, LogEvent event) throws Exception {
+        StringBuilder builder = new StringBuilder();
+        builder.append(event.getReceivedTimestamp());
+        builder.append(" [");
+        builder.append(event.getSource().toString());
+        builder.append("] [");
+        builder.append(event.getLogfile());
+        builder.append("] : ");
+        builder.append(event.getMsg());
+        System.out.println(builder.toString());
+    }
+}
