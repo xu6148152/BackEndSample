bf1f31ac58ddb2f3cf5c24a9e6a62ba1c54795b6
xu6148152 <xu6148152@gmail.com>
2018-02-22 21:54:55 +0800

    Add session sample

---
+++ GoSample/src/sample/sessionSample.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"html/template"
+	"net/http"
+	"net/url"
+	"sync"
+	"time"
+	"container/list"
+)
+
+var globalSessions *Manager
+
+func init() {
+	go globalSessions.GC()
+	globalSessions, _ = NewManager("memory", "gosessionid", 3600)
+}
+
+type Manager struct {
+	cookieName  string
+	lock        sync.Mutex
+	provider    Provider
+	maxLifeTime int64
+}
+
+type Provider interface {
+	SessionInit(sid string) (Session, error)
+	SessionRead(sid string) (Session, error)
+	SessionDestroy(sid string) error
+	SessionGC(maxLifeTime int64)
+}
+
+type Session interface {
+	Set(key, value interface{}) error
+	Get(key interface{}) interface{}
+	Delete(key interface{}) error
+	SessionID() string
+}
+
+var provides = make(map[string]Provider)
+
+func NewManager(provideName, cookieName string, maxLifeTime int64) (*Manager, error) {
+	provider, ok := provides[provideName]
+	if !ok {
+		return nil, fmt.Errorf("session: unknown provide %q (forgotten import?)", provideName)
+	}
+	return &Manager{provider: provider, cookieName: cookieName, maxLifeTime: maxLifeTime}, nil
+}
+
+func Register(name string, provider Provider) {
+	if provider == nil {
+		panic("session: Register provider is nil")
+	}
+	if _, dup := provides[name]; dup {
+		panic("session: Register called twice for provider " + name)
+	}
+	provides[name] = provider
+}
+
+func (manager *Manager) sessionId() string {
+	b := make([]byte, 32)
+	if _, err := rand.Read(b); err != nil {
+		return ""
+	}
+	return base64.URLEncoding.EncodeToString(b)
+}
+
+func (manager *Manager) SessionStart(w http.ResponseWriter, r *http.Request) (session Session) {
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+	cookie, err := r.Cookie(manager.cookieName)
+	if err != nil || cookie.Value == "" {
+		sid := manager.sessionId()
+		session, _ = manager.provider.SessionInit(sid)
+		cookie := http.Cookie{Name: manager.cookieName, Value: url.QueryEscape(sid), Path: "/",
+			HttpOnly: true, MaxAge: int(manager.maxLifeTime)}
+		http.SetCookie(w, &cookie)
+	} else {
+		sid, _ := url.QueryUnescape(cookie.Value)
+		session, _ = manager.provider.SessionRead(sid)
+	}
+	return
+}
+
+func count(w http.ResponseWriter, r *http.Request) {
+	sess := globalSessions.SessionStart(w, r)
+	createtime := sess.Get("createtime")
+	if createtime == nil {
+		sess.Set("createtime", time.Now().Unix())
+	} else if createtime.(int64)+360 < time.Now().Unix() {
+		globalSessions.SessionDestroy(w, r)
+		sess = globalSessions.SessionStart(w, r)
+	}
+	ct := sess.Get("countnum")
+	if ct == nil {
+		sess.Set("countnum", 1)
+	} else {
+		sess.Set("countnum", ct.(int)+1)
+	}
+	t, _ := template.ParseFiles("count.gtpl")
+	w.Header().Set("Content-Type", "text/html")
+	t.Execute(w, sess.Get("countnum"))
+}
+
+func (manager *Manager) SessionDestroy(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie(manager.cookieName)
+	if err != nil || cookie.Value == "" {
+		return
+	} else {
+		manager.lock.Lock()
+		defer manager.lock.Unlock()
+		manager.provider.SessionDestroy(cookie.Value)
+		expiration := time.Now()
+		cookie := http.Cookie{Name: manager.cookieName, Path: "/", HttpOnly: true, Expires: expiration, MaxAge: -1}
+		http.SetCookie(w, &cookie)
+	}
+}
+
+func (manager *Manager) GC() {
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+	manager.provider.SessionGC(manager.maxLifeTime)
+	time.AfterFunc(time.Duration(manager.maxLifeTime), func() { manager.GC() })
+}
+
+var pder = &Prover{list: list.New()}
+
+type SessionStore struct {
+	sid          string
+	timeAccessed time.Time
+	value        map[interface{}]interface{}
+}
+
+func (st *SessionStore) Set(key, value interface{}) error {
+	st.value[key] = value
+	pder.SessionUpdate(st.sid)
+	return nil
+}
+
+func (st *SessionStore) Get(key interface{}) interface{} {
+	pder.SessionUpdate(st.sid)
+	if v, ok := st.value[key]; ok {
+		return v
+	} else {
+		return nil
+	}
+}
+
+func (st *SessionStore) Delete(key interface{}) error {
+	delete(st.value, key)
+	pder.SessionUpdate(st.sid)
+	return nil
+}
+
+func (st *SessionStore) SessionID() string {
+	return st.sid
+}
+
+//type Provider struct {
+//	lock     sync.Mutex
+//	sessions map[string]*list.Element
+//	list     *list.List
+//}
+
+//func (pder *Provider) SessionInit(sid string) (Session, error) {
+//
+//}
--- GoSample/src/sample/webSample.go
+++ GoSample/src/sample/webSample.go
@@ -42,25 +42,30 @@ func sayHelloName(w http.ResponseWriter, r *http.Request) {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
+	sess := globalSessions.SessionStart(w, r)
+	r.ParseForm()
 	fmt.Println("method: ", r.Method)
 	if r.Method == "GET" {
-		crutime := time.Now().Unix()
-		h := md5.New()
-		io.WriteString(h, strconv.FormatInt(crutime, 10))
-		token := fmt.Sprintf("%x", h.Sum(nil))
+		//crutime := time.Now().Unix()
+		//h := md5.New()
+		//io.WriteString(h, strconv.FormatInt(crutime, 10))
+		//token := fmt.Sprintf("%x", h.Sum(nil))
 		t, _ := template.ParseFiles("./login.gtpl")
-		log.Println(t.Execute(w, token))
+		w.Header().Set("Content-Type", "text/html")
+		log.Println(t.Execute(w, sess.Get("username")))
 	} else {
-		r.ParseForm()
-		token := r.Form.Get("token")
-		if token != "" {
-
-		} else {
-
-		}
-		fmt.Println("username: ", template.HTMLEscapeString(r.Form.Get("username")))
-		fmt.Println("password: ", template.HTMLEscapeString(r.Form.Get("password")))
-		template.HTMLEscape(w, []byte(r.Form.Get("username")))
+		sess.Set("username", r.Form["username"])
+		http.Redirect(w, r, "/", 302)
+		//r.ParseForm()
+		//token := r.Form.Get("token")
+		//if token != "" {
+		//
+		//} else {
+		//
+		//}
+		//fmt.Println("username: ", template.HTMLEscapeString(r.Form.Get("username")))
+		//fmt.Println("password: ", template.HTMLEscapeString(r.Form.Get("password")))
+		//template.HTMLEscape(w, []byte(r.Form.Get("username")))
 	}
 }
 
