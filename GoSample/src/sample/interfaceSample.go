d20556d0ac02575138514518c31bf4e58fcdccd2
xu6148152 <xu6148152@gmail.com>
2018-02-25 17:07:26 +0800

    Update interfaceSample

--- GoSample/src/sample/interfaceSample.go
+++ GoSample/src/sample/interfaceSample.go
@@ -2,25 +2,24 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 type Stringer interface {
 	String() string
 }
 
-func (h Human) Sing(lyrics string) {
-	fmt.Println("La la la la...", lyrics)
-}
-
-func (h Human) String() string {
-	return "❰" + h.name + " - " + strconv.Itoa(h.age) + " years -  ✆ " + h.phone + "❱"
-}
-
-func (e Employee) SayHi() {
-	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n",
-		e.name, e.company, e.phone)
-}
+//func (h Human) Sing(lyrics string) {
+//	fmt.Println("La la la la...", lyrics)
+//}
+//
+//func (h Human) String() string {
+//	return "❰" + h.name + " - " + strconv.Itoa(h.age) + " years -  ✆ " + h.phone + "❱"
+//}
+//
+//func (e Employee) SayHi() {
+//	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n",
+//		e.name, e.company, e.phone)
+//}
 
 type Men interface {
 	SayHi()
@@ -30,24 +29,119 @@ type Men interface {
 type Element interface{}
 type List []Element
 
-func testValueElement() {
-	list := make(List, 3)
-	list[0] = 1
-	list[1] = "Hello"
-	list[2] = person{"Dennis", 70}
+//func testValueElement() {
+//	list := make(List, 3)
+//	list[0] = 1
+//	list[1] = "Hello"
+//	list[2] = person{"Dennis", 70}
+//
+//	for index, element := range list {
+//		if value, ok := element.(int); ok {
+//			fmt.Printf("list[%d] is an int and its value is %d\n", index, value)
+//		} else if value, ok := element.(string); ok {
+//			fmt.Printf("list[%d] is a string and its value is %s\n", index, value)
+//		} else if value, ok := element.(person); ok {
+//			fmt.Printf("list[%d] is a Person and its value is %s\n", index, value)
+//		} else {
+//			fmt.Printf("list[%d] is of a different type\n", index)
+//		}
+//	}
+//}
 
-	for index, element := range list {
-		if value, ok := element.(int); ok {
-			fmt.Printf("list[%d] is an int and its value is %d\n", index, value)
-		} else if value, ok := element.(string); ok {
-			fmt.Printf("list[%d] is a string and its value is %s\n", index, value)
-		} else if value, ok := element.(person); ok {
-			fmt.Printf("list[%d] is a Person and its value is %s\n", index, value)
-		} else {
-			fmt.Printf("list[%d] is of a different type\n", index)
-		}
+type Animal interface {
+	Speak() string
+}
+
+type Dog struct{}
+
+func (d Dog) Speak() string {
+	return "wangwang!"
+}
+
+type Cat struct{}
+
+func (c Cat) Speak() string {
+	return "miao"
+}
+
+type Pig struct{}
+
+func (p *Pig) Speak() string {
+	return "keng"
+}
+
+type Programmer struct{}
+
+func (p Programmer) Speak() string {
+	return "Hello, world"
+}
+
+func testAnimal() {
+	animals := []Animal{Dog{}, Cat{}, &Pig{}, Programmer{}}
+	for _, animal := range animals {
+		fmt.Println(animal.Speak())
 	}
 }
+
+type I interface {
+	Get() string
+	Set(string)
+}
+
+type S struct {
+	val string
+}
+
+type T struct {
+	val string
+}
+
+func (s S) Get() string {
+	return s.val
+}
+
+func (s *S) Set(val string) {
+	s.val = val
+}
+
+func (t T) Get() string {
+	return t.val
+}
+
+func (t *T) Set(val string) {
+	t.val = val
+}
+
+func testInterface() {
+	var i I = &S{}
+	i.Set("Hello")
+	fmt.Println(i.Get())
+
+	if t, ok := i.(*S); ok {
+		fmt.Println("s implements I", t)
+	}
+
+	switch t := i.(type) {
+	case *T:
+		fmt.Println("i store *T", t)
+	case *S:
+		fmt.Println("i store *S", t)
+	}
+}
+
+func printAll(vals []interface{}) {
+	for _, val := range vals {
+		fmt.Println(val)
+	}
+}
+
+func testInterfaceSlice() {
+	vals := []string{"stanley", "david", "oscar"}
+	printAll(vals)
+}
+
 func main() {
-	testValueElement()
+	//testValueElement()
+	//testInterface()
+	testInterfaceSlice()
 }
